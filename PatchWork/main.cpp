454fd7e7e047982655fb3724ef22d3810dd4dba7
Git-Huy <chinook_@hotmail.fr>
2016-04-09 14:09:55 +0200

    point class

--- PatchWork/main.cpp
+++ PatchWork/main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
+#include "point.h"
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {
     cout << "Hello World!" << endl;
+    Point p(1,2);
+
     return 0;
 }
--- PatchWork/point.h
+++ PatchWork/point.h
@@ -1,11 +1,20 @@
 #ifndef POINT_H
 #define POINT_H
+#include <iostream>
+
+using namespace std;
 
 class Point
 {
     public:
-        Point();
+        Point(int x,int y);
         ~Point();
+        int getX() const;
+        int getY() const;
+        void setX(int x);
+        void setY(int y);
+
+        friend ostream& operator<< (ostream &os, const Point &p);
 
     private :
         int x,y;
