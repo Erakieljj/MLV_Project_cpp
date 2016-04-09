33b9c91326e5c8a3724f5e7d7cc9fce164055a7d
Peroumalle <mperouma@gmail.com>
2016-04-09 16:35:06 +0200

    typo error

--- PatchWork/fresque.cpp
+++ PatchWork/fresque.cpp
@@ -1,50 +1,56 @@
 #include "fresque.h"
 using namespace std;
 
+Fresque::Fresque() {
+    cout << "Init fresque";
+}
+
 // Inherited method from ObjectInterface
-void draw(){
+void Fresque::draw(){
     for(ObjectInterface o : objects) {
         o.draw();
     }
 }
 
-void applyTranslation(float x,float y){
+void Fresque::applyTranslation(float x,float y){
     for(ObjectInterface o : objects) {
         o.applyTranslation(x,y);
     }
 }
-void applyRotationDirect(float angle){
+void Fresque::applyRotationDirect(float angle){
     for(ObjectInterface o : objects) {
         o.applyRotationDirect(angle);
     }
 }
-void applyRotationIndirect(float angle){
+void Fresque::applyRotationIndirect(float angle){
     for(ObjectInterface o : objects) {
         o.applyRotationIndirect(angle);
     }
 }
-void applyHomethety(float ratio){
+void Fresque::applyHomethety(float ratio){
     for(ObjectInterface o : objects) {
         o.applyHomethety(ratio);
     }
 }
-void applyAxialSymmetry(){
+void Fresque::applyAxialSymmetry(){
     for(ObjectInterface o : objects) {
         o.applyAxialSymmetry();
     }
 }
 
-void applyCentralSymmetry(){
+void Fresque::applyCentralSymmetry(){
     for(ObjectInterface o : objects) {
         o.applyCentralSymmetry();
     }
 }
 
 // Personal method
-bool add(ObjectInterface o){
+bool Fresque::add(ObjectInterface o){
     objects.push_back(o);
+    return true;
 }
 
-bool remove(ObjectInterface o){
-    std:cout << "Remove";
+bool Fresque::remove(ObjectInterface o){
+    cout << "Remove";
+    return false;
 }
--- PatchWork/fresque.h
+++ PatchWork/fresque.h
@@ -6,22 +6,23 @@
 
 class Fresque : public ObjectInterface
 {
-public:
-    Fresque();
-    std::vector<ObjectInterface> objects;
-    // Inherited method from ObjectInterface
+    public:
+        Fresque();
+        // Inherited method from ObjectInterface
+        void draw();
+        void applyTranslation(float x,float y);
+        void applyRotationDirect(float angle);
+        void applyRotationIndirect(float angle);
+        void applyHomethety(float ratio);
+        void applyAxialSymmetry();
+        void applyCentralSymmetry();
 
-    void draw();
-    void applyTranslation(float x,float y);
-    void applyRotationDirect(float angle);
-    void applyRotationIndirect(float angle);
-    void applyHomethety(float ratio);
-    void applyAxialSymmetry();
-    void applyCentralSymmetry();
+        // Personal method
+        bool add(ObjectInterface o);
+        bool remove(ObjectInterface o);
 
-    // Personal method
-    bool add(ObjectInterface o);
-    bool remove(ObjectInterface o);
+    private:
+        std::vector<ObjectInterface> objects;
 };
 
 #endif // FRESQUE_H
