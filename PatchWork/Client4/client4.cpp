27f83d1779f164365f91e2bc462590471f033bd9
Git-Huy <chinook_@hotmail.fr>
2016-04-14 16:06:48 +0200

    major improvement on comments : massive deletes :+1:

--- PatchWork/Client4/client4.cpp
+++ PatchWork/Client4/client4.cpp
@@ -36,32 +36,8 @@ int main()
 {
     /* ---------- INITIALIZING VARIABLES ---------- */
 
-    /*
-       1. client is a file descriptor to store the values
-       returned by the socket system call and the accept
-       system call.
-       2. portNum is for storing port number on which
-       the accepts connections
-       3. exit is bool variable which will be used to
-       end the loop
-       4. The client reads characters from the socket
-       connection into a dynamic variable (buffer).
-       5. A sockaddr_in is a structure containing an internet
-       address. This structure is already defined in netinet/in.h, so
-       we don't need to declare it again.
-        DEFINITION:
-        struct sockaddr_in
-        {
-          short   sin_family;
-          u_short sin_port;
-          struct  in_addr sin_addr;
-          char    sin_zero[8];
-        };
-        6. serv_addr will contain the address of the server
-    */
-
     int client;
-    int portNum = 1500; // NOTE that the port number is same for both client and server
+    int portNum = 1500; // le port est le même pour le client et le serveur
     bool finished = false;
     int bufsize = 3000;
     char buffer[bufsize];
@@ -77,7 +53,6 @@ int main()
     client = socket(AF_INET, SOCK_STREAM, 0);
 
     /* ---------- ESTABLISHING SOCKET CONNECTION ----------*/
-    /* --------------- socket() function ------------------*/
 
     if (client < 0)
     {
@@ -85,28 +60,7 @@ int main()
         exit(1);
     }
 
-    /*
-        The socket() function creates a new socket.
-        It takes 3 arguments,
-            a. AF_INET: address domain of the socket.
-            b. SOCK_STREAM: Type of socket. a stream socket in
-            which characters are read in a continuous stream (TCP)
-            c. Third is a protocol argument: should always be 0. The
-            OS will choose the most appropiate protocol.
-            This will return a small integer and is used for all
-            references to this socket. If the socket call fails,
-            it returns -1.
-    */
-
     cout << "\n>> Socket client has been created..." << endl;
-
-    /*
-        The variable serv_addr is a structure of sockaddr_in.
-        sin_family contains a code for the address family.
-        It should always be set to AF_INET.
-        htons() converts the port number from host byte order
-        to a port number in network byte order.
-    */
 
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(portNum);
@@ -114,7 +68,6 @@ int main()
 
 
     /* ---------- CONNECTING THE SOCKET ---------- */
-    /* ---------------- connect() ---------------- */
 
     if (connect(client,(struct sockaddr *)&server_addr, sizeof(server_addr)) != 0) {
         cout << ">> Connection error" << endl;
@@ -128,19 +81,6 @@ int main()
     else {
         cout << ">> Connection to the server port number: " << portNum << endl;
     }
-
-    /*
-        The connect function is called by the client to
-        establish a connection to the server. It takes
-        three arguments, the socket file descriptor, the
-        address of the host to which it wants to connect
-        (including the port number), and the size of this
-        address.
-        This function returns 0 on success and -1
-        if it fails.
-        Note that the client needs to know the port number of
-        the server but not its own port number.
-    */
 
     cout << ">> Awaiting confirmation from the server..." << endl; //line 40
     recv(client, buffer, bufsize, 0);
@@ -182,7 +122,6 @@ int main()
     } while (!finished);
 
     /* ---------------- CLOSE CALL ------------- */
-    /* ----------------- close() --------------- */
     cout << "\n>> Connection terminated.\n";
 
     close(client);
