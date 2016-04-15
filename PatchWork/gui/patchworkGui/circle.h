050f61d59aa016ad57772ad3d68d505259f15f20
Git-Huy <chinook_@hotmail.fr>
2016-04-15 19:25:39 +0200

    comments

--- PatchWork/gui/patchworkGui/circle.h
+++ PatchWork/gui/patchworkGui/circle.h
@@ -8,6 +8,9 @@
 #include <QObject>
 using namespace  std;
 
+/**
+ * @brief La classe Circle permet de créer un cercle avec une couleur, un centre de coordonnées x et y, un rayon.
+ */
 class Circle : public Object2D
 {
 Q_OBJECT
@@ -15,15 +18,44 @@ private:
     double rayon;
     QGraphicsScene *scene;
     public:
+
         Circle(string color,Point p1,double rayon,QGraphicsScene *scene);
 
         // Inherited method from Object2D
+        /**
+         * @brief fonction qui calcule l'aire du cercle.
+         * @return Retourne l'aire du cercle.
+         */
         float getArea();
+
+        /**
+         * @brief fonction qui calcule le périmètre du cercle.
+         * @return Retourne le périmètre du cercle.
+         */
         float getPerimeter();
+
+        /**
+         * @brief fonction qui set le rayon du cercle.
+         * @param x Taille du rayon.
+         * @return Retourne le périmètre du cercle.
+         */
         void setRayon(double x){rayon*=x;}
 
+        /**
+         * @brief draw Dessine la forme du cercle.
+         */
         void draw();
+
+        /**
+         * @brief fonction qui détermine si l'objet est un cercle.
+         * @return retourne vrai si l'objet est un cercle sinon faux.
+         */
         bool isCircle(){return true;}
+
+        /**
+         * @brief fonction qui récupère le centre du cercle.
+         * @return retourne un point qui correspond au centre du cercle.
+         */
         Point getCenter();
         double getRayon(){return rayon;}
 
